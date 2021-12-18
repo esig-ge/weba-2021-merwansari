31c15ad1c642d7379a9a30145cdc290d3d824a87
merwansari <merwan.sr@eduge.ch>
2021-12-18 23:27:23 +0100

    Fin de mon popup dans Camps Discover

--- Projet/EDW_Academy/static/camps/SelectedCampPopup.js
+++ Projet/EDW_Academy/static/camps/SelectedCampPopup.js
@@ -1,1 +1,19 @@
 
+function showPopup(content, id) {
+  const popup = document.createElement("div");
+  popup.className = "informations-popup";
+  popup.id = id;
+  popup.innerHTML = content;
+  document.body.append(popup);
+}
+
+function hidePopup(popup) {
+  popup.remove();
+}
+
+document.getElementById("campslist").onchange = function() {
+  showPopup("<h1>Discover Camps</h1><br><h4> Discover camp's price are always 150 CHF and they are here to make you discover new games. </h4>", "informations-popup");
+  setTimeout(function() {
+    hidePopup(document.getElementById("informations-popup"));
+  }, 4000);
+}
